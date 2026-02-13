6b82feecf396012a77bab8469849a9e68801f954
KIMATHI <fkimathi833@gmail.com>
2026-02-13 22:20:29 +0300

    Add Car class extending Vehicle with fuelType field

---
+++ car.java
@@ -0,0 +1,18 @@
+/ Car.java
+public class Car extends Vehicle {
+    
+    // Additional field
+    private String fuelType;
+
+    // Constructor
+    public Car(String brand, String model, int year, String fuelType) {
+        super(brand, model, year);  // Call parent constructor
+        this.fuelType = fuelType;
+    }
+
+    // Override displayDetails method
+    @Override
+    public void displayDetails() {
+        super.displayDetails();  // Call parent method
+        System.out.println("Fuel Type: " + fuelType);
+    }
